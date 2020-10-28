47a267e6b2aced189a6ff8c608e57199c1e17bb1
KleyLima <kley158@gmail.com>
2020-10-28 13:11:35 -0300

    Feat: Basic functions with dao

---
+++ source/dao/address_dao.py
@@ -0,0 +1,21 @@
+from source.dao.models.address import Address
+
+
+def insert_address(address, number, cep, complement, state, city, neighborhood):
+    addr = Address(address=address, number=number, cep=cep, complement=complement, state=state, city=city,
+                   neighborhood=neighborhood)
+    return addr.save()
+
+
+def delete_address(address, number, cep, complement, state, city, neighborhood):
+    pass
+
+
+def select_by_address(address):
+    return Address.select(Address.cep).where(Address.address == address).get()
+
+
+if __name__ == "__main__":
+    # insert_address("Rua CAntareira", 123, "789465-12", "Bloco 4", "SP", "OZ", "OZ")
+    # print("Created")
+    print(vars(select_by_address("Rua CAntareira")))
