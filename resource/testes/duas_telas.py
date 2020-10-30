400604919549e0e28ea19563ca2f59e023bcc3b6
root <ricardo.goncalves7@fatec.sp.gov.br>
2020-10-30 20:36:03 -0300

    criação forms endereço e proprietario

--- resource/testes/duas_telas.py
+++ resource/testes/duas_telas.py
@@ -6,11 +6,13 @@ from PyQt5 import uic, QtWidgets
 def chama_segunda_tela():
     primeira_tela.hide()
     segunda_tela.show()
+    terceira_tela.show()
 
 
 app = QtWidgets.QApplication([])
 primeira_tela = uic.loadUi("untitled.ui")
-segunda_tela = uic.loadUi("sencond.ui")
+segunda_tela = uic.loadUi("form_prop.ui")
+terceira_tela = uic.loadUi("form_ender.ui")
 
 primeira_tela.pushButton.clicked.connect(chama_segunda_tela)
 
