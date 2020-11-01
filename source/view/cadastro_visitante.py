21abf3cb6e1462e21b2239e5285695e1fff15a2d
KleyLima <kley158@gmail.com>
2020-10-31 22:27:31 -0300

    new widgets

--- source/view/cadastro_visitante.py
+++ source/view/cadastro_visitante.py
@@ -4,11 +4,7 @@ from resource.testes.telas import app, form_visitante
 from PyQt5.QtCore import Qt
 from source.utils.centralizar_tela import location_on_the_screen
 
-
-def teste():
-    print(999999)
-
-
-form_visitante.salvar_visi.clicked.connect(teste)
+# Example of a funcion assigned to a button:
+# form_visitante.salvar_visi.clicked.connect(teste)
 form_visitante.setWindowFlag(Qt.FramelessWindowHint)
 location_on_the_screen(form_visitante)
