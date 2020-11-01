eae40a63841c1c85d116825327ecc360dbe8c375
KleyLima <kley158@gmail.com>
2020-10-31 22:10:26 -0300

    helper to center widgets

---
+++ source/utils/centralizar_tela.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QDesktopWidget
+
+
+def location_on_the_screen(tela):
+    """
+    Wanna a window centred on screen? here is your saviour!
+    :param tela: Screen to be centred. Is a PyQt5 QtWidget object
+    :return: None
+    """
+    screen = QDesktopWidget().screenGeometry()
+    widget = tela.geometry()
+    x = screen.width() - widget.width()
+    y = screen.height() - widget.height()
+    tela.move(x/2, y/2)
