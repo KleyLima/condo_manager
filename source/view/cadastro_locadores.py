2ecfe2597525e5b0f49f02e7009bb72a364e8a54
Gustavo <gustavo.torquato96@gmail.com>
2020-11-04 15:23:30 -0300

    Teste

--- source/view/cadastro_locadores.py
+++ source/view/cadastro_locadores.py
@@ -4,7 +4,7 @@ from resource.testes.telas import app, locador
 from PyQt5.QtCore import Qt
 from source.utils.centralizar_tela import location_on_the_screen
 
-
+print("Teste")
 
 locador.setWindowFlag(Qt.FramelessWindowHint)
 location_on_the_screen(locador)
