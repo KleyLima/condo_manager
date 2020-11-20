da72d78d7804cf048bf89513ec1a3b238d7161bc
KleyLima <kley158@gmail.com>
2020-11-20 09:04:35 -0300

    feat: Implemented the use of the utils to back to menu and clear fields

--- source/view/controle_material.py
+++ source/view/controle_material.py
@@ -2,17 +2,13 @@
 
 from resource.telas.inicializacao import app, controle_material, menu
 from PyQt5.QtCore import Qt
-from source.utils.screen_utils import center_screen
-
-# Example of a funcion assigned to a button:
+from source.utils.screen_utils import center_screen, clear_and_exit
 
 
-def teste():
-    print("MENU")
-    controle_material.hide()
-    menu.show()
+def back_menu():
+    clear_and_exit(controle_material)
 
 
-controle_material.b_menu.clicked.connect(teste)
+controle_material.b_menu.clicked.connect(back_menu)
 controle_material.setWindowFlag(Qt.FramelessWindowHint)
 center_screen(controle_material)
