67a21eda806ace729b7afe4e0c65c21aacb2e78f
Ricardo <ricardo.goncalves7@fatec.sp.gov.br>
2020-12-06 21:22:30 -0300

    function: update query in material_dao.

--- source/dao/material_dao.py
+++ source/dao/material_dao.py
@@ -2,8 +2,6 @@
 
 
 from source.dao.models.materiais import Materiais
-from source.dao.visitante_dao import select_by_name
-
 
 
 def insert_material(nome_produto, qtd):
@@ -19,3 +17,30 @@ def insert_material(nome_produto, qtd):
 def select_by_material():
     by_name = Materiais.select().where(Materiais.product_name != "").get()
     return by_name
+
+
+def update_add_material(produto, qtd):
+    upd = Materiais.update(product_qtd= Materiais.product_qtd + qtd).where(Materiais.product_name == produto)
+    return upd
+
+def update_rem_material(produto, qtd):
+    upd = Materiais.update(product_qtd= Materiais.product_qtd - qtd).where(Materiais.product_name == produto)
+    return upd
+
+
+def select_all():
+    by_name = Materiais.select().where(Materiais.product_name != "")
+    return by_name
+
+
+
+# if __name__ == '__main__':
+
+#     """
+#         Caso queira testar os valores dos itens apos update ou remove.
+#     """
+#     lista = select_all()
+#     print(lista)
+
+#     for qtd in lista:
+#         print(qtd.product_qtd)
