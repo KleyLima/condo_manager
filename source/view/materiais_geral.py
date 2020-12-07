f51e83e6164dbb302578b987879b4823fb7b8e7b
Ricardo <ricardo.goncalves7@fatec.sp.gov.br>
2020-12-06 21:22:52 -0300

    function: update fields complete.

--- source/view/materiais_geral.py
+++ source/view/materiais_geral.py
@@ -2,10 +2,11 @@ from resource.telas.inicializacao import app, controle_material, menu, adicionar
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QLineEdit
 from source.controller.material_controller import validate
-from source.dao.material_dao import select_by_material
+from source.dao.material_dao import select_by_material, update_add_material, update_rem_material
 from source.utils.screen_utils import center_screen, clear_input_boxes, back_to_menu
 
-# inicializaçao da base de proprietarios no combo box.
+
+# inicializaçao da base de produtos no combo box de adicionar.
 di = {}
 def load_combo_add():
 
@@ -16,6 +17,7 @@ def load_combo_add():
         adicionar_material.combo_materiais.addItem(nome.product_name)
 
 
+# inicializaçao da base de produtos no combo box de remover.
 di2 = {}
 def load_combo_rem():
 
@@ -26,6 +28,7 @@ def load_combo_rem():
         deletar_material.combo_materiais.addItem(nome.product_name)
 
 
+# Limpeza de cx de texto e retorno para pagina de controle de materiais
 def clear_and_exit(frame):
 
     inputs = frame.findChildren(QLineEdit)
@@ -65,11 +68,27 @@ def cadas_material():
 
 # Function that do a update in database, adding qtd in a product list
 def add_material():
-    pass
+    produto = adicionar_material.combo_materiais.currentText()
+    qtd = adicionar_material.line_qtd_material.text()
+
+    upd = update_add_material(produto, int(qtd))
+    upd.execute()
+    print("update complete...")
+    clear_and_exit(adicionar_material)
+
+    
+
 
 # Function that do a update in database, removing qtd in a product list
 def rem_material():
-    pass
+
+    produto = deletar_material.combo_materiais.currentText()
+    qtd = deletar_material.line_qtd_material.text()
+
+    upd = update_rem_material(produto, int(qtd))
+    upd.execute()
+    print("update complete...")
+    clear_and_exit(deletar_material)
 
 
 
@@ -77,6 +96,8 @@ def rem_material():
 
 # Function buttons and more
 cadastrar_material.cadastrar_material.clicked.connect(cadas_material)
+adicionar_material.add_material.clicked.connect(add_material)
+deletar_material.del_material.clicked.connect(rem_material)
 cadastrar_material.cancel.clicked.connect(back_cadasMaterial)
 adicionar_material.cancel.clicked.connect(back_addMaterial)
 deletar_material.cancel.clicked.connect(back_remMaterial)
