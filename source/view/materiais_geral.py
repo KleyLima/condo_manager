f0f9f4de58eb70cbf0323015dbfdd0472d775563
KleyLima <kley158@gmail.com>
2020-12-06 23:24:15 -0300

    feat: update the materials list after a operation

--- source/view/materiais_geral.py
+++ source/view/materiais_geral.py
@@ -4,10 +4,13 @@ from PyQt5.QtWidgets import QLineEdit
 from source.controller.material_controller import validate
 from source.dao.material_dao import select_by_material, update_add_material, update_rem_material
 from source.utils.screen_utils import center_screen, clear_input_boxes, back_to_menu
+from source.view.controle_material import att_list
 
 
 # inicializaçao da base de produtos no combo box de adicionar.
 di = {}
+
+
 def load_combo_add():
 
     lista_nome = select_by_material()
@@ -19,6 +22,8 @@ def load_combo_add():
 
 # inicializaçao da base de produtos no combo box de remover.
 di2 = {}
+
+
 def load_combo_rem():
 
     lista_nome = select_by_material()
@@ -44,29 +49,27 @@ def clear_and_exit(frame):
 
 def back_cadasMaterial():
     clear_and_exit(cadastrar_material)
-    
 
 
 def back_addMaterial():
     clear_and_exit(adicionar_material)
-   
-    
+
 
 def back_remMaterial():
     clear_and_exit(deletar_material)
-    
-        
+
+
 # Function to register a product in database
 def cadas_material():
     nome_produto = cadastrar_material.line_nome_material.text()
     qtd = cadastrar_material.line_qtd_material.text()
-    
 
     if validate(nome_produto, qtd):
         load_combo_add()
         load_combo_rem()
         clear_and_exit(cadastrar_material)
-     
+        att_list()
+
 
 # Function that do a update in database, adding qtd in a product list
 def add_material():
@@ -76,10 +79,9 @@ def add_material():
     upd = update_add_material(produto, int(qtd))
     upd.execute()
     print("update complete...")
-    
-    clear_and_exit(adicionar_material)
 
-    
+    clear_and_exit(adicionar_material)
+    att_list()
 
 
 # Function that do a update in database, removing qtd in a product list
@@ -91,11 +93,9 @@ def rem_material():
     upd = update_rem_material(produto, int(qtd))
     upd.execute()
     print("update complete...")
-    
+
     clear_and_exit(deletar_material)
-
-
-
+    att_list()
 
 
 # Function buttons and more
